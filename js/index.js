2c6cefbb123df692d4fafe5b00e232abd06c6101
Joachim Robert <joachim.robert@actiane.com>
2017-03-06 14:42:35 +0100

    Tiny JS tweaks.

--- js/index.js
+++ js/index.js
@@ -1,19 +1,15 @@
 $(document).ready(function() {
 
-  $("#sensorselect").on("change", function(e) {
-    $(".invisible").show();
-    updateFullframe();
-    updateSensor();
-  })
-
   function updateFullframe() {
     if ($("#fullframe")[0].value) {
       if ($("#fullframe")[0].value.indexOf("-") > -1) {
         result = "";
         $("#fullframe")[0].value.split("-").forEach(function(i, j) {
-          nombre = (isNaN(parseInt(i)) ? 0 : i);
-          result += Math.round(parseInt(nombre) * Math.sqrt(parseInt($("#sensorselect")[0].value)) / Math.sqrt(864));
-          result += (j == 0 ? "-" : "");
+          if(j <= 1) {
+            itemValue = (isNaN(parseInt(i)) ? 0 : i);
+            result += Math.round(parseInt(itemValue) * Math.sqrt(parseInt($("#sensorselect")[0].value)) / Math.sqrt(864));
+            result += (j == 0 ? "-" : "");
+          }
         });
         $("#fullframeresult").text(result + "mm zoom lens");
       }
@@ -30,9 +26,11 @@ $(document).ready(function() {
       if ($("#sensor")[0].value.indexOf("-") > -1) {
         result = "";
         $("#sensor")[0].value.split("-").forEach(function(i, j) {
-          nombre = (isNaN(parseInt(i)) ? 0 : i);
-          result += Math.round(parseInt(nombre) * Math.sqrt(864) / Math.sqrt(parseInt($("#sensorselect")[0].value)));
-          result += (j == 0 ? "-" : "");
+          if(j <= 1) {
+            itemValue = (isNaN(parseInt(i)) ? 0 : i);
+            result += Math.round(parseInt(itemValue) * Math.sqrt(864) / Math.sqrt(parseInt($("#sensorselect")[0].value)));
+            result += (j == 0 ? "-" : "");
+          }
         });
         $("#sensorresult").text(result + "mm zoom lens");
       }
@@ -43,6 +41,12 @@ $(document).ready(function() {
     }
 
   }
+
+  $("#sensorselect").on("change", function(e) {
+    $(".invisible").show();
+    updateFullframe();
+    updateSensor();
+  })
 
   $("#fullframe").on("input", function(e) {
     updateFullframe()
